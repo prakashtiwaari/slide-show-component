44b2f3e016910d44e098c80d5cc5dddf2cbdea1e
prakashtiwaari <prakash@github.com>
2022-05-14 23:05:03 +0530

    feat: sliding animation

--- src/components/slide-show/slider-container.js
+++ src/components/slide-show/slider-container.js
@@ -3,45 +3,13 @@ import Slide from "./slide";
 
 const SliderContainer = ({firstSlideName, secondSlideName, thirdSlideName}) => {
     const colors = ["#FFBB28", "#00C49F", "#FFBB28"];
-    const [firstSlide, setFirstSlide] = useState(true);
-    const [secondSlide, setSecondSlide] = useState(true);
-    const [thirdSlide, setThirdSlide] = useState(false);
 
-    function callBack3() {
-        setThirdSlide(true);
-        setSecondSlide(false);
-        setTimeout(callBack, 1000);
-    }
-
-    function callBack2() {
-        setFirstSlide(false);
-        setSecondSlide(true);
-        setTimeout(callBack3, 1000);
-    }
-
-    const callBack = () => {
-        console.log('here')
-        setSecondSlide(false);
-        setThirdSlide(false);
-        setTimeout(callBack2, 1000)
-    };
-
-
-    callBack();
 
     return (
         <div className='slideshow-slider'>
-            {
-                firstSlide && <Slide slideName={firstSlideName} backgroundColor={colors[0]}/>
-            }
-
-            {
-                secondSlide && <Slide slideName={secondSlideName} backgroundColor={colors[1]}/>
-            }
-            {
-                thirdSlide && <Slide slideName={thirdSlideName} backgroundColor={colors[2]}/>
-            }
-
+            <Slide slideName={firstSlideName} backgroundColor={colors[0]}/>
+            <Slide slideName={secondSlideName} backgroundColor={colors[1]}/>
+            <Slide slideName={thirdSlideName} backgroundColor={colors[2]}/>
         </div>
     );
 }
