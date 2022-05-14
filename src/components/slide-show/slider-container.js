51265b0c8dd020693690e0044b50844823639e59
prakashtiwaari <prakash@github.com>
2022-05-14 22:59:03 +0530

    feat: sliding animation

--- src/components/slide-show/slider-container.js
+++ src/components/slide-show/slider-container.js
@@ -7,6 +7,27 @@ const SliderContainer = ({firstSlideName, secondSlideName, thirdSlideName}) => {
     const [secondSlide, setSecondSlide] = useState(true);
     const [thirdSlide, setThirdSlide] = useState(false);
 
+    function callBack3() {
+        setThirdSlide(true);
+        setSecondSlide(false);
+        setTimeout(callBack, 1000);
+    }
+
+    function callBack2() {
+        setFirstSlide(false);
+        setSecondSlide(true);
+        setTimeout(callBack3, 1000);
+    }
+
+    const callBack = () => {
+        setSecondSlide(false);
+        setThirdSlide(false);
+        setTimeout(callBack2, 1000)
+    };
+
+
+    callBack();
+
     return (
         <div className='slideshow-slider'>
             {
